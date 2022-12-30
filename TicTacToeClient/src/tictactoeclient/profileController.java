d5590229e98d533386ee37e12680ca2131290522
saraeltlt <47868870+saraeltlt@users.noreply.github.com>
2022-12-30 23:41:24 +0200

    profile window link

--- TicTacToeClient/src/tictactoeclient/profileController.java
+++ TicTacToeClient/src/tictactoeclient/profileController.java
@@ -7,6 +7,7 @@ package tictactoeclient;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -57,6 +58,14 @@ public class profileController {
      
         stage.showAndWait(); 
         
+    }
+
+    public void switchToHome(ActionEvent event ) throws IOException{
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("HomePage.FXML")));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     
     private Label label;
