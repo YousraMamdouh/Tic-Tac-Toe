8a1c04d596b5bf147eda561d4fde149cd5c5ef96
Ramy Ashraf <ramy3r@hotmail.com>
2022-12-30 17:35:43 +0200

    Enhanced Buttons Visuals

--- TicTacToeClient/src/tictactoeclient/DifficultyWindowController.java
+++ TicTacToeClient/src/tictactoeclient/DifficultyWindowController.java
@@ -1,36 +1,33 @@
 package tictactoeclient;
 
 import javafx.fxml.FXML;
-import javafx.scene.text.Text;
 
 public class DifficultyWindowController {
+    private int difficultyLevel;
 
-    @FXML
-    private Text levelText;
     @FXML
     private void easy() {
 
         System.out.println("I hEaRd SoMeOnE ClIcK tHe EaSy BuTtOn !");
-        levelText.setText("Easy");
-
+        difficultyLevel = 1;
+        System.out.println("Difficulty Level Has Been Set To "+ difficultyLevel);
     }
 
     @FXML
     private void medium() {
         System.out.println("I hEaRd SoMeOnE ClIcK tHe MeDiUm BuTtOn !");
-        levelText.setText("Medium");
+        difficultyLevel = 2;
+        System.out.println("Difficulty Level Has Been Set To "+ difficultyLevel);
     }
 
     @FXML
     private void hard() {
         System.out.println("I hEaRd SoMeOnE ClIcK tHe HaRd BuTtOn !");
-        levelText.setText("Hard");
+        difficultyLevel = 3;
+        System.out.println("Difficulty Level Has Been Set To "+ difficultyLevel);
     }
 
-    @FXML
-    private void play() {
-        System.out.println("NoW We'Re PlAyInG !");
-    }
+
     @FXML
     private void back(){
         //TODO: 12/30/2022 Link this function with the previous window
