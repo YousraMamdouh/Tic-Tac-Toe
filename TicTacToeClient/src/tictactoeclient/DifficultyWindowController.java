07e5ec25c9de714348457e3a8e4ffe1257aeb966
saraeltlt <47868870+saraeltlt@users.noreply.github.com>
2023-01-04 20:56:55 +0200

    Revert ""hard level player""

--- TicTacToeClient/src/tictactoeclient/DifficultyWindowController.java
+++ TicTacToeClient/src/tictactoeclient/DifficultyWindowController.java
@@ -52,7 +52,7 @@ public class DifficultyWindowController {
 
 
     private void toGameBoard(ActionEvent e) throws IOException {
-        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GameBoardHard.FXML")));
+        root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GameBoard.FXML")));
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
