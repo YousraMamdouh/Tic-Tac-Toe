56e5284fd11a05ce51b45060d39468d684db9e1a
Ramy Ashraf <ramy3r@hotmail.com>
2023-01-06 15:50:29 +0200

    Added DatabaseConnection class

--- TicTacToeServer/src/TicTacToeServer/TicTacToeServer.java
+++ TicTacToeServer/src/TicTacToeServer/TicTacToeServer.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+
 import java.util.Objects;
 
 public class TicTacToeServer extends Application {
@@ -29,6 +30,29 @@ public class TicTacToeServer extends Application {
     }
 
     public static void main(String[] args) {
+        /*Testing database operations insert and select statements using DTOs*/
+//        try {
+//            DatabaseConnection.registerPlayer(new Player("TestSubject1", "TestSubject1@Domain.com", "TestSubject1'sPassword"));
+//            DatabaseConnection.registerPlayer(new Player("TestSubject2", "TestSubject2@Domain.com", "TestSubject2'sPassword"));
+//        } catch (SQLException e) {
+//            System.out.println("User Name Already In Database");
+//        }
+//        try {
+//            DatabaseConnection.insertGame(new GameHistory(22, 23, new int[]{8, 7, 6, 5, 4, 3, 2, 1, 0}, 22));
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//                List<Player> players = DatabaseConnection.getPlayerList();
+//        for (Player player : players) {
+//            System.out.println(player.getPlayer_id() + " - " + player.getName() + " " +
+//                    player.getScore() + " " + player.getEmail() + ":" + player.getPassword());
+//        }
+//        List<GameHistory> gameHistoryList = DatabaseConnection.getGameHistoryList();
+//        for (GameHistory game : gameHistoryList) {
+//            System.out.println(game.getPlayerOne() + " vs " + game.getPlayerTwo() + " " + game.getGame_id() + " winner ->> " +
+//                    game.getWinnerName() + " Time : " + game.getDate());
+//        }
+
         launch(args);
     }
 
